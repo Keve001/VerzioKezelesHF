07dc093a63e7ebc63353ba4ee1826d26f34c9fb1
Petrik Lajos Szg <info@petrik.hu>
2020-09-23 12:56:36 +0200

    Add project files.

---
+++ VerzioKezelesHF/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VerzioKezelesHF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int szam = 0;
+            int[] t = new int[10];
+            do
+            {
+                Console.WriteLine("Adja meg a számot!");
+                int sajat = Convert.ToInt32(Console.ReadLine());
+                szam++;
+
+            } while (szam !=10);
+        }
+    }
+}
